45c14598cb450f050898cc55418a3c2fd708fb1d
Jonathan Dubin <mail@jondubin.com>
2015-06-02 21:21:00 -0400

    Modularized the config.

---
+++ personal.el
@@ -0,0 +1,32 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+
+;; loads zenburn
+(load-theme 'zenburn)
+
+
+;; ace jump mode major function
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+
+;; remap M-x to C-x C-m
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+
+;; etc remaps
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
